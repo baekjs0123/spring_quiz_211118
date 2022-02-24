d993fb6c3164ee69fe1f2938a76dd6e139afe7aa
DESKTOP-43O7LPO\baekj <baekjs0123@naver.com>
2022-02-25 01:29:03 +0900

    baekjs0123/spring_quiz_211118#4 lesson04 quiz01 3번 풀이시 2번에서 풀었던 기존 메소드를 모두 재사용하려고해보았으나 실패하고 Controller의 메소드만 재활용하고 나머지 메소드들은 새로 만들어서 문제 해결.

---
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -0,0 +1,54 @@
+package com.quiz.lesson04;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.lesson04.bo.SellerBO;
+import com.quiz.lesson04.model.Seller;
+
+@RequestMapping("/lesson04/quiz01")
+@Controller
+public class Lesson04Quiz01Controller {
+	
+	@Autowired
+	private SellerBO sellerBO;
+	
+	// seller 추가
+	// 입력 form URL : http://localhost/lesson04/quiz01/1
+	@RequestMapping("/1")
+	public String quiz01_1() {
+		// add_seller.jsp
+		return "lesson04/add_seller";
+	}
+	
+	// 요청 URL : http://localhost/lesson04/quiz01/add_seller
+	@PostMapping("/add_seller")
+	public String addSeller(
+			@RequestParam("nickname") String nickname,
+			@RequestParam(value="profileImageUrl", required=false) String profileImageUrl,
+			@RequestParam("temperature") double temperature) {
+		
+		// DB insert
+		sellerBO.addSeller(nickname, profileImageUrl, temperature);
+		
+		// response: view 경로
+		return "lesson04/after_add_seller";
+	}
+	
+	// 요청 URL : http://localhost/lesson04/quiz01/seller_info
+	// 요청 URL : http://localhost/lesson04/quiz01/seller_info?id=1
+	@RequestMapping("/seller_info")
+	public String sellerInfo(Model model, @RequestParam(value="id", defaultValue="6") int id) {
+		// Seller seller = sellerBO.getLastSeller();
+		Seller seller = sellerBO.getSeller(id);
+		model.addAttribute("seller", seller);
+		model.addAttribute("subject", "판매자 정보");
+		
+		return "lesson04/seller_info";
+	}
+}

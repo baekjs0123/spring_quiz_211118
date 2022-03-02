8b823f9dfb305736cbfb1b2160e5f8327792d3f7
DESKTOP-43O7LPO\baekj <baekjs0123@naver.com>
2022-03-02 20:43:59 +0900

    lesson05 quiz03

--- src/main/java/com/quiz/lesson05/Lesson05Controller.java
+++ src/main/java/com/quiz/lesson05/Lesson05Controller.java
@@ -17,8 +17,9 @@ public class Lesson05Controller {
 		return "lesson05/quiz01";
 	}
 	
-	@RequestMapping("/lesson05/quiz02_1")
+	@RequestMapping("/lesson05/quiz02")
 	public String quiz02_1(Model model) {
+		// 문제1
 		List<String> musicRanking = new ArrayList<>();
 		musicRanking.add("강남스타일");
 		musicRanking.add("벚꽃엔딩");
@@ -28,7 +29,47 @@ public class Lesson05Controller {
 		
 		model.addAttribute("musicRanking", musicRanking);
 		
-		return "lesson05/quiz02_1";
+		// 문제2
+		List<Map<String, Object>> membership = new ArrayList<>();
+
+		Map<String, Object> member = new HashMap<>();
+		member.put("name", "손오공");
+		member.put("phoneNumber", "010-1234-5678");
+		member.put("grade", "VIP");
+		member.put("point", 12040);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "저팔계");
+		member.put("phoneNumber", "010-4321-1234");
+		member.put("grade", "BASIC");
+		member.put("point", 3420);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "사오정");
+		member.put("phoneNumber", "010-8888-1111");
+		member.put("grade", "GOLD");
+		member.put("point", 1530);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "삼장");
+		member.put("phoneNumber", "010-0000-1234");
+		member.put("grade", "GOLD");
+		member.put("point", 8450);
+		membership.add(member);
+
+		member = new HashMap<>();
+		member.put("name", "우마왕");
+		member.put("phoneNumber", "010-1111-2222");
+		member.put("grade", "BASIC");
+		member.put("point", 420);
+		membership.add(member);
+		
+		model.addAttribute("membership", membership);
+		
+		return "lesson05/quiz02";
 	}
 	
 	@RequestMapping("/lesson05/quiz02_2")
@@ -73,5 +114,46 @@ public class Lesson05Controller {
 		model.addAttribute("membership", membership);
 		
 		return "lesson05/quiz02_2";
+	}
+	
+	@RequestMapping("/lesson05/quiz03_1")
+	public String quiz03_1(Model model) {
+		List<Integer> candidates = new ArrayList<>();
+		candidates.add(263001);
+		candidates.add(173942); 
+		candidates.add(563057);
+		
+		model.addAttribute("candidates", candidates);
+		
+		return "lesson05/quiz03_1";
+	}
+	
+	@RequestMapping("/lesson05/quiz03_2")
+	public String quiz03_2(Model model) {
+		List<Map<String, Object>> cardBills = new ArrayList<>();
+
+		Map<String, Object> cardBill = new HashMap<>();
+		cardBill.put("store", "GS48");
+		cardBill.put("pay", 7800);
+		cardBill.put("date", "2025-09-14");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "현태백화점");
+		cardBill.put("pay", 2750000);
+		cardBill.put("date", "2025-09-18");
+		cardBill.put("installment", "3개월");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "요촌치킨");
+		cardBill.put("pay", 180000);
+		cardBill.put("date", "2025-09-20");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+		
+		model.addAttribute("cardBills", cardBills);
+		return "lesson05/quiz03_2";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/lesson05/quiz03_1.jsp
+++ src/main/webapp/WEB-INF/jsp/lesson05/quiz03_1.jsp
@@ -1,11 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>lesson05 quiz02_1</title>
+<title>lesson05 quiz03</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -13,19 +14,21 @@
 </head>
 <body>
 	<div class="container">
-		<h1>HOT5</h1>
+		<h1>1. 후보자 득표율</h1>
 		<table class="table text-center">
 			<thead>
 				<tr>
-					<th>순위</th>
-					<th>제목</th>
-				</tr>	
+					<th>기호</th>
+					<th>득표 수</th>
+					<th>득표 율</th>
+				</tr>
 			</thead>
 			<tbody>
-			<c:forEach var="musicRanking" items="${musicRanking}" varStatus="status">
+			<c:forEach var="candidate" items="${candidates}" varStatus="status">
 				<tr>
 					<td>${status.count}</td>
-					<td>${musicRanking}</td>
+					<td><fmt:formatNumber value="${candidate}" type="number" /></td>
+					<td><fmt:formatNumber value="${candidate / 1000000}" type="percent" /></td>
 				</tr>
 			</c:forEach>
 			</tbody>
